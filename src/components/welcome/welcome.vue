2ee64448a85fba59353ddce51f416290e6f36ca7
Gordan Pendic <gordan.pendic5@gmail.com>
2018-09-02 20:30:20 +0200

    Sign Up gone...

--- src/components/welcome/welcome.vue
+++ src/components/welcome/welcome.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="login-html">
             <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
-            <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+            <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab"><!--Sign Up--></label>
             <div class="login-form">
                 <div class="sign-in-htm">
                     <div class="group">
@@ -48,7 +48,7 @@
                         <router-link to="/reset-form">Forgot Password?</router-link>
                     </div>
                 </div>
-                <div class="sign-up-htm">
+                <div class="sign-up-htm" v-if="false">
                     <div class="group">
                         <label for="user2" class="label">Username</label>
                         <input id="user2" type="text" class="input">
